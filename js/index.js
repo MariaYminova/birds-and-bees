f857e12b2a3c793510edd7633bfa0edb603ebc83
MariaYminova <vabylia96@gmail.com>
2022-11-09 19:05:03 +0300

    all blocks have been added

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@
 const swiper = new Swiper('.swiper', {
   loop: true,
 
-
   pagination: {
     el: '.pagination-slider__bottom',
     clickable: true,
@@ -11,11 +10,29 @@ const swiper = new Swiper('.swiper', {
     
   },
 
- 
   navigation: {
     nextEl: '.m-swiper-button-next',
     prevEl: '.m-swiper-button-prev',
   },
 
+
+});
+
+const swiperEvents = new Swiper(".swiper-events", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  centeredSlides: false,
+
+  scrollbar: {
+    el: ".pagination-point",
+    hide: true,
+    type: 'custom',
+    
+  },
+
+  navigation: {
+    nextEl: '.events-swiper-button-next',
+    prevEl: '.events-swiper-button-prev',
+  },
 
 });
